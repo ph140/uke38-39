7bef1e523d83fbab79767e8cc8a65edd2aa49d8b
ph140 <petterh140@gmail.com>
2020-09-07 11:47:34 +0200

    Endret en kommentar

--- opg3.py
+++ opg3.py
@@ -1,11 +1,11 @@
 # Tar inn tre tall fra brukeren
 a = abs(int(input("Tall1: ")))
-b = abs(int(input("Tall2: ")))
-c = abs(int(input("Tall3: ")))
+b = abs(float(input("Tall2: ")))
+c = abs(float(input("Tall3: ")))
 
 
 def triple(a, b, c):
-    # Endrer variablene til å være opphøyd i andre
+    # Endrer variablene til kvadratet av seg selv
     a = a**2
     b = b**2
     c = c**2
