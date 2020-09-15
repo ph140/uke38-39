b38870654b6606b21fdd72939396ee172ec4fcf3
ph140 <petterh140@gmail.com>
2020-09-15 19:30:14 +0200

    Fjernet unødvenfig str()

--- opg2.py
+++ opg2.py
@@ -1,21 +1,9 @@
 # Tar inn input fra brukeren.
-navn = str(input("Navn: "))
+navn = input("Navn: ")
 alder = int(input("Alder: "))
 
 
-class person():
-    # Oppretter en klasse som tar navn og alder.
-    def __init__(self, navn, alder):
-        self.navn = navn
-        self.alder = alder
-
-    def ermyndig(cls):  # Sjekker alder og om personen kan kjøre eller ikke.
-        if cls.alder >= 18:
-            return cls.navn + ", du er gammel nok til å kjøre."
-        else:
-            return cls.navn + ", du er dessverre for ung til å kjøre."
-
-
-nyperson = person(navn, alder)  # lager ny person med input fra tidligere
-
-print(nyperson.ermyndig())
+if alder > 18:
+    print(f"Hei å hopp {navn}, du er gammel nok til å kjøre!")
+else:
+    print(f"Hei å hopp {navn}, du er for ung til å kjøre!")
